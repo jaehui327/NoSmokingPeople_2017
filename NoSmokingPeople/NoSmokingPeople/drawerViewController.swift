bf198465c539c31f1de43477e2cd4ddad41d6c18
김재희 <jaehui327@naver.com>
2018-02-02 13:42:40 +0900

    autoLayout and KYDrawer

---
+++ NoSmokingPeople/NoSmokingPeople/drawerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  drawerViewController.swift
+//  NoSmokingPeople
+//
+//  Created by 김재희 on 2018. 2. 2..
+//  Copyright © 2018년 김재희. All rights reserved.
+//
+
+import UIKit
+
+class drawerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
